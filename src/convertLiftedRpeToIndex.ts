18d0d9f5c23d3d385c030575c49ba0870667bb01
Melisa-Zorraindo <melisazorraindo@gmail.com>
2026-08-26 18:04:26 +0200

    removes keyword return

--- src/convertLiftedRpeToIndex.ts
+++ src/convertLiftedRpeToIndex.ts
@@ -1,5 +1,4 @@
 import { MINIMUM_RPE } from "./constants.js"
 
-export const convertLiftedRpeToIndex = (liftedRpe: number): number => {
-  return Math.round((liftedRpe - MINIMUM_RPE) / 0.5)
-}
+export const convertLiftedRpeToIndex = (liftedRpe: number): number =>
+  Math.round((liftedRpe - MINIMUM_RPE) / 0.5)
